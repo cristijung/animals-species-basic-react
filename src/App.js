527228dcc4412e3d7fc4833ae25777f5cf94a4d4
Cristina Jung <cristijung@gmail.com>
2022-12-05 17:06:40 -0300

    peixes

--- src/App.js
+++ src/App.js
@@ -117,33 +117,33 @@ function App() {
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'Gato/Felino - Maine Coon',
-      cargo: 'Maine Coon é uma raça de gato originária do nordeste dos Estados Unidos.',
-      imagem: 'https://mega.ibxk.com.br/2016/08/23/23184723908456.jpg?ims=610x',
+      nome: 'Cavalo Marinho',
+      cargo: 'São peixes ósseos do gênero Hippocampus são cheios de peculiaridades: além da morfologia irreverente, recordando um cavalo, as espécies deste gênero apresentam cuidado parental realizado pelo macho, que carrega em sua bolsa incubadora os ovos fecundados.',
+      imagem: 'https://www.infoescola.com/wp-content/uploads/2008/05/cavalo-marinho-109485932-480x600.jpg',
       time: times[2].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'Cachorro/Canino - Dachshund',
-      cargo: 'Dachshund ou teckel, também apelidado de cão-salsicha, é uma raça de cães oriunda da Alemanha.',
-      imagem: 'https://www.petz.com.br/cachorro/racas/dachshund/img/dachshund-guia-racas.webp',
+      nome: 'Kinguio Telescópio',
+      cargo: 'O kinguio telescópio é conhecido como peixe-telescópio ou peixe dragão. Sua origem é chinesa, mas também foi uma espécie bastante desenvolvida no Japão por meio de várias técnicas de reprodução. Atualmente, o Japão conta com 30 variedades de kinguio.',
+      imagem: 'https://pm1.narvii.com/6981/23b83440a31458e3bb1ff84115249a4f6c225bccr1-427-640v2_hq.jpg',
       time: times[2].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'Lobo - Canino',
-      cargo: 'É parente bem próximo do cão doméstico e acredita-se que este tenha se originado através da domesticação de lobos cinzentos. ',
-      imagem: '	https://www.infoescola.com/wp-content/uploads/2008/05/lobo-360x450.jpg',
+      nome: 'Polvo',
+      cargo: 'Os polvos são moluscos da classe Cephalopoda. Esses animais destacam-se por sua forma peculiar e por apresentar incríveis meios de defesa. Estima-se que existam, no mínimo, 100 espécies diferentes de polvos.',
+      imagem: 'https://img.freepik.com/fotos-premium/um-polvo-gigante-do-oceano-ilustracoes-3d_533101-92.jpg?w=2000',
       time: times[2].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'Raposa - Canino',
-      cargo: 'A raposa tem uma aparência que lembra os cachorros de estimação. Alguns tipos podem apresentar um perigo para o ser humano é necessário ficar um pouco distante para a sua própria proteção.',
-      imagem: 'http://4.bp.blogspot.com/-JW33BOQRVpE/UwxXYVpmXFI/AAAAAAAAA9M/aF_a4vKcf3U/s1600/Feneco+II.jpg',
+      nome: 'Tubarão',
+      cargo: 'Tubarão-branco é uma espécie de tubarão lamniforme. Trata-se do maior peixe predador existente nos oceanos. Os tubarões brancos são bem conhecidos por seu tamanho e fama de assassino.',
+      imagem: 'https://i.pinimg.com/564x/dc/33/35/dc33359eca7e0fb321d52800bfcfa14f--shark-jaws-sharks.jpg',
       time: times[2].nome
     },
     {
